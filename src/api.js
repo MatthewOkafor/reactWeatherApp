d5650ee489349cac4785a810e369899ceeb3dc9c
Matthew Okafor <marymatt009@gmail.com>
2022-10-03 11:20:27 +0100

    my changes

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '46063aa742msh85f395057bfedf4p1dbd03jsn6f3041b94eb6',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "3beb35c3058dbc92edaf20a3bb0cf62e";
